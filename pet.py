8ce47b49bed188d0f454e5c6afeb5168981dcac6
P3df4r <pedfar321@gmail.com>
2024-06-09 18:42:04 -0300

    Story2

--- pet.py
+++ pet.py
@@ -1,15 +1,14 @@
 import docker
 from pymongo import MongoClient
 
-def register_pet(nome, idade_anos, idade_meses, raca, especie, sexo, alergia, objeto_acompanhado, laudos_anteriores, historico_doencas):
+def register_pet(nome, idade_anos, idade_meses, raca, especie, sexo: str, alergia: list, objeto_acompanhado: list, receitas_anteriores: dict, procedimentos_realizados: dict, consultas_realizadas: list):
     client = docker.DockerClient()
     container = client.containers.get('pet_heath_mongodb')
     ip_add = container.attrs['NetworkSettings']['IPAddress']
-    client_mongo = MongoClient(ip_add) #Porta padrão para o biomics
-    #client_mongo_database = client_mongo.test
+    client_mongo = MongoClient(ip_add) 
     petdatabase = client_mongo["petdatabase"]
     petdata = petdatabase["petdata"]
-    credenciais = {"nome": nome, "idade_anos": idade_anos, "idade_meses": idade_meses, "raca": raca, "especie": especie, "sexo": sexo, "alergia": alergia, "objeto_acompanhado": objeto_acompanhado, "laudos_anteriores": laudos_anteriores, "historico_doencas": historico_doencas}
+    credenciais = {"nome": nome, "idade_anos": idade_anos, "idade_meses": idade_meses, "raca": raca, "especie": especie, "sexo": sexo, "alergia": alergia, "objeto_acompanhado": objeto_acompanhado, "receitas_anteriores": receitas_anteriores, "procedimentos_realizados": procedimentos_realizados, "consultas_realizadas": consultas_realizadas}
     petdata.insert_one(credenciais)
 
 def register_tutor(tutor, idade_tutor, endereco_tutor, telefone_tutor):
@@ -18,14 +17,53 @@ def register_tutor(tutor, idade_tutor, endereco_tutor, telefone_tutor):
     endereco_tutor= endereco_tutor
     telefone_tutor = telefone_tutor
 
+def register_consult(id_paciente, diagnostico: list, date: int):
+    client = docker.DockerClient()
+    container = client.containers.get('pet_heath_mongodb')
+    ip_add = container.attrs['NetworkSettings']['IPAddress']
+    client_mongo = MongoClient(ip_add) 
+    petdatabase = client_mongo["petdatabase"]
+    petconsult = petdatabase["petconsult"]
+    consulta = {"_id": id_paciente, "diagnostico": diagnostico, "date": date}
+    petconsult.insert_one(consulta)
+
+def register_anamenase(id_paciente, last_anamnase):
+    client = docker.DockerClient()
+    container = client.containers.get('pet_heath_mongodb')
+    ip_add = container.attrs['NetworkSettings']['IPAddress']
+    client_mongo = MongoClient(ip_add) 
+    petdatabase = client_mongo["petdatabase"]
+    petanamnase = petdatabase["petanamnase"]
+    anamnase = {"_id": id_paciente, "anamnase": last_anamnase}
+    petanamnase.insert_one(anamnase)
+
 def altered_info_pet(campo, info):
     client = docker.DockerClient()
     container = client.containers.get('pet_heath_mongodb')
     ip_add = container.attrs['NetworkSettings']['IPAddress']
-    client_mongo = MongoClient(ip_add) #Porta padrão para o biomics
-    #client_mongo_database = client_mongo.test
+    client_mongo = MongoClient(ip_add) 
     petdatabase = client_mongo["petdatabase"]
     petdata = petdatabase["petdata"]
     credenciais = {campo: info}
     petdata.insert_one(credenciais)
+
+def see_infos(id):
+    client = docker.DockerClient()
+    container = client.containers.get('pet_heath_mongodb')
+    ip_add = container.attrs['NetworkSettings']['IPAddress']
+    client_mongo = MongoClient(ip_add) 
+    petdatabase = client_mongo["petdatabase"]
+    petdata = petdatabase["petdata"]
+    credenciais = {"_id": id}
+    petdata.find(credenciais)
+
+def create_recipe(id, indicacoes: dict):
+    client = docker.DockerClient()
+    container = client.containers.get('pet_heath_mongodb')
+    ip_add = container.attrs['NetworkSettings']['IPAddress']
+    client_mongo = MongoClient(ip_add) 
+    petdatabase = client_mongo["petdatabase"]
+    petdata = petdatabase["petdata"]
+    receitas = {"_id": id,  "receitas_anteriores":indicacoes}
+    petdata.insert_one(receitas)
 
--- vet.py
+++ vet.py
@@ -1,1 +1,33 @@
-a
+from pymongo import MongoClient
+import docker
+
+def check_consult_date(id_paciente):
+    client = docker.DockerClient()
+    container = client.containers.get('pet_heath_mongodb')
+    ip_add = container.attrs['NetworkSettings']['IPAddress']
+    client_mongo = MongoClient(ip_add) #Porta padrão para o biomics
+    petdatabase = client_mongo["petdatabase"]
+    petconsult = petdatabase["petconsult"]
+    consulta = {"id": id}
+    date = petconsult.find(consulta)
+    if date:
+        return date
+    else:
+        return False
+
+def remenber_consult(id_paciente):
+    client = docker.DockerClient()
+    consult_date = check_consult_date(id_paciente)
+    if consult_date:
+        container = client.containers.get('pet_heath_mongodb')
+        ip_add = container.attrs['NetworkSettings']['IPAddress']
+        client_mongo = MongoClient(ip_add) #Porta padrão para o biomics
+        petdatabase = client_mongo["petdatabase"]
+        petconsult = petdatabase["petconsult"]
+        consulta = {"date": consult_date}
+        proximity = petconsult.find(consulta)
+        if proximity:
+            return True
+        else:
+            return False
+ 
