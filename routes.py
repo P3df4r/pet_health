83a69ca9617e4f0ec0e98730fbb62656cdd3226a
p3df4r <pedfar321@gmail.com>
2024-10-21 20:45:05 -0300

    Rotas modificadas
    Rotas modificadas de acordo com os dados solicitados no banco de dados.
    Se for considerar como crud. Estarei alterando o back para adequar os
    dados modificados nas rotas

--- routes.py
+++ routes.py
@@ -6,103 +6,60 @@ app = Flask(__name__)
 
 @app.route('/addpet', methods=['GET', 'POST'])
 def dados():
-    nome = ''
-    idade_anos = 0
-    idade_meses = 0
-    raca = ""
-    especie = ""
-    sexo = False
-    alergia = ""
-    objeto_acompanhado = ""
-    receitas_anteriores = {} 
-    procedimentos_realizados = {} 
-    consultas_realizadas = [] 
-    return pet.register_pet(nome,idade_anos,idade_meses,raca,especie,sexo,alergia,objeto_acompanhado,receitas_anteriores,procedimentos_realizados, consultas_realizadas)
+    data = request.get_json()
+    nome = data['nome']
+    idade = data['idade']
+    peso = data['idade']
+    especie = data['especie']
+    id_tutor = data['ID_tutor']
+    sexo = data['sexo']
+    return 0
 
 @app.route('/rm_pet', methods=['GET', 'POST'])
 def rmpet():
-    id = ''
+    data = request.get_json()
+    id = data['id'] 
     teste = pet.remove_pet(id)
     return teste
 
-@app.route('/alt_pet', methods=['GET', 'POST'])
-def altpet():
-    campo = ''
-    info = ''
-    id = 0
-    return pet.altered_info_pet(campo, info, id)
-
 @app.route('/add_tutor', methods=['GET', 'POST'])
 def reg_tutor():
-    tutor = ''
-    idade_tutor = 0
-    endereco_tutor = ''
-    telefone = ''
+    data = request.get_json()
+    tutor = data['nome']
+    endereco_tutor = data['endereco_tutor']
+    telefone = data['telefone']
+    email = data['email']
+    cpf = data['cpf']
     return pet.register_tutor(tutor, idade_tutor, endereco_tutor, telefone)
 
 @app.route('/rm_tutor', methods=['GET', 'POST'])
 def rmtutor():
-    id = ''
+    data = request.get_json()
+    id = data['id']
     return pet.remove_tutor(id)
 
-@app.route('/alt_tutor', methods=['GET', 'POST'])
-def alttutor():
-    campo = ''
-    info = ''
-    id = 0
-    return pet.altered_info_tutor(campo, info, id)
-
-
-@app.route('/add_relationship', methods=['GET', 'POST'])
-def addrel():
-    id_pet = ''
-    id_tutor = ''
-    return pet.register_relationship(id_tutor, id_pet)
-
-#@app.route('/rm_relatioship')
-#def rm_relationship();
-#    id = ''
-#    return 
-
-#@app.route('/alt_relationship')
-
-@app.route('/add_consult', methods=['GET', 'POST'])
-def addconsult():
-    id = ''
-    data = datetime.datetime(2019, 12, 4)
-    return pet.register_consult(id, data)
-
-#@app.route('/rm_consult')
-
-@app.route('/alt_consult', methods=['GET', 'POST'])
-def alt_consult():
-    id = ''
-    campo = ''
-    new_info = ''
-    return pet.altered_consult(campo, new_info, id)
-
 @app.route('/add_anamenase', methods=['GET', 'POST'])
-def addanamenase():
-    id_paciente = ''
-    last_anamenase = ''
+def addficha():
+    data = request.get_json()
+    id_paciente = data['id_tutelado']
+    obs = data['observacoes']
+    id_hist_clin = data['id_hist_clin']
     return pet.register_anamenase(id_paciente,last_anamenase)
 
 @app.route('/see_info_pet', methods=['GET', 'POST'])
-def seeinfopet():
-    nome = ''
-    teste = pet.see_infos_pet(nome)
+def hist_clin():
+    data = request.get_json()
+    alergia = data['alergia']
+    cirurgia = data['cirurgia']
+    exame = data['exame']
+    medicamento = data['medicamento']
+    suplementacao = data['suplementacao']
     return teste
 
 @app.route('/see_info_tutor', methods=['GET', 'POST'])
 def seeinfotutor():
     nome = ''
     return pet.see_infos_tutor(nome)
-
-@app.route('/create_recipe', methods=['GET', 'POST'])
-def createrecipe():
-    id = ''
-    recipe = {}
-    return pet.create_recipe(id, recipe)
 
 @app.route('/teste', methods=['GET', 'POST'])
 def teste():
